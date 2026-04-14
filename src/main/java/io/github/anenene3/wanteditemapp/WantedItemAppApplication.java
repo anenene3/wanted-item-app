b7976cb28641df44d325d3b699e2f09bcfe8f3fa
anenene3 <218555645+anenene3@ users.noreply.github.com>
2026-04-14 10:47:41 +0900

    Initial commit

---
+++ src/main/java/io/github/anenene3/wanteditemapp/WantedItemAppApplication.java
@@ -0,0 +1,14 @@
+package io.github.anenene3.wanteditemapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedItemAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedItemAppApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/io/github/anenene3/wanteditemapp/WantedItemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.anenene3.wanteditemapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedItemAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
