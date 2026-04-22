386230bb806b1ddb922fc55b1cfd21b4cf0629dc
anenene3 <218555645+anenene3@ users.noreply.github.com>
2026-04-22 16:49:44 +0900

    Add frontent item list screen

---
+++ frontend/src/App.jsx
@@ -0,0 +1,45 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main">
+      <h1>募集一覧</h1>
+
+      <div className="item-list">
+        <div className="item-card">
+          <div className="item-image">写真</div>
+          <div className="item-price">5000円</div>
+          <div className="item-name">ゲームソフトA</div>
+        </div>
+
+        <div className="item-card">
+          <div className="item-image">写真</div>
+          <div className="item-price">8000円</div>
+          <div className="item-name">ゲームソフトB</div>
+        </div>
+
+        <div className="item-card">
+          <div className="item-image">写真</div>
+          <div className="item-price">8000円</div>
+          <div className="item-name">ゲームソフトB</div>
+        </div>
+
+        <div className="item-card">
+          <div className="item-image">写真</div>
+          <div className="item-price">8000円</div>
+          <div className="item-name">ゲームソフトB</div>
+        </div>
+
+        <div className="item-card">
+          <div className="item-image">写真</div>
+          <div className="item-price">8000円</div>
+          <div className="item-name">ゲームソフトB</div>
+        </div>
+
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
