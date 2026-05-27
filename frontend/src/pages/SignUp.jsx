b1014ed7cff6f0957bde9de16d7f21bd26cdbd18
anenene3 <218555645+anenene3@ users.noreply.github.com>
2026-05-27 15:48:29 +0900

    アカウント登録のフロント側を実装

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,28 +1,113 @@
 import "../App.css";
 import Header from "../components/Header";
 import {useNavigate} from "react-router";
+import { useState } from "react";
 
 function SignUp(){
   const navigate=useNavigate();
+
+  const [userName, setUserName] = useState("");
+  const [loginId, setLoginId] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmationPassword, setConfirmationPassword] = useState("");
+
+  const handleSignUp = () => {
+     if (!userName.trim()) {
+      alert("ユーザーネームを入力してください");
+      return;
+    }
+
+    if (!loginId.trim()) {
+      alert("ユーザーIDを入力してください");
+      return;
+    }
+
+    if (!password.trim()) {
+      alert("パスワードを入力してください");
+      return;
+    }
+
+    if (!confirmationPassword.trim()) {
+      alert("確認用パスワードを入力してください");
+      return;
+    }
+
+    if (password !== confirmationPassword) {
+      alert("パスワードと確認用パスワードが一致していません");
+      return;
+    }
+
+
+    fetch("http://localhost:8080/accounts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        userName: userName,
+        loginId: loginId,
+        password: password
+      })
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        if (data === "登録成功") {
+          console.log("登録成功:", data);
+          navigate("/login");
+          return;
+        }
+
+        if (data === "ログインID重複") {
+          alert("そのユーザーIDは既に使われています");
+          return;
+        }
+
+        alert("登録に失敗しました");
+      })
+  };
+
 
   return(
     <div className="signup">
       <Header />
       <h1>新規アカウント登録</h1>
       <div className="signup-contents">
+
         <p>ユーザーネーム：</p>
-        <input className="signup-user-name"/>
+        <input 
+          className="signup-user-name"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+
         <p>ユーザーID:</p>
-        <input className="signup-user-id"/>
+        <input 
+          className="signup-user-id"
+          value={loginId}
+          onChange={(e) => setLoginId(e.target.value)}
+        />
+
         <p>パスワード:</p>
-        <input className="signup-password" type="password"/>
+        <input 
+          className="signup-password" 
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
         <p>パスワード（確認用）:</p>
-        <input className="signup-confirm-password" type="password"/>
+        <input 
+          className="signup-confirm-password" 
+          type="password"
+          value={confirmationPassword}
+          onChange={(e) => setConfirmationPassword(e.target.value)}
+        />
+
         <input
           className="signup-send-button"
           type="button"
           value="新規登録"
-          onClick={() => navigate("/login")}
+          onClick={handleSignUp}
         />
 
         <input
